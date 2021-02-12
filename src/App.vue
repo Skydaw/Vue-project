4e17d9e9ee6d0bb4f8fabc35b83c72b9f6150435
Sébastien Avenet <sebastien.avenet1@gmail.com>
2021-02-12 13:31:34 +0100

    suite cours

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,101 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+    <Logo bienvenue="Message de bienvenue" v-on:sendBounce="bounces ++" :bounces="bounces"/>
+    <h1>Intro à Vue.js</h1>
+    <div class="counter">
+         Counter :{{counter}}
+    </div>
+    <button @click='clicked' class="clicks">
+          Clicks :{{clicks}}
+    </button>
+    <div id="conditional">
+      <span v-if="clicks>=10">calme</span>
+    </div>
+    <h2> {{message}} </h2>
+    <input v-model="message"/>
+    <ul id="array-rendering">
+    <li v-for="item in items" :key='item.name' @click='test'>
+      {{item.name}}only for {{item.price}}
+    </li>
+    </ul>
+
+
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Logo from './components/Logo' 
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Logo,
+      },
+  data(){
+    return{
+      counter:0,
+      clicks:0,
+      message: "Salut !",
+
+      items: [
+        {name :  'TV LG15ED56 4K', price:"899€"},
+        {name :  'Sony Playstation 5', price:"599€"},
+        {name :  'Xbox Series X', price:"499€"},
+        {name :  'Apple iPhone 12', price:"999€"},
+        {name :  'samsung Galaxy S21', price:"899€"},
+        {name :  'TV Samsung 1H 4K', price:"999€"}
+      ],
+     bounces:0,
+
+    }
+  },
+  mounted(){
+    setInterval(()=>{
+      this.counter++
+    },1000)
+  },
+  methods :{
+    clicked(){
+      this.clicks++;
+    },
+    test(){
+      console.log(this)
+    },
+    
+
+
   }
 }
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+*{
+    list-style: none;
+    text-align: center;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    margin: 5px;
+    padding: 0;
 }
+ul{
+  transform: translateX(25%);
+  width: 15%;
+  margin: auto;
+}
+li{
+  text-align: left;
+}
+li:hover{
+  background-color:black;
+  color: white;
+  transition: all 0.5s ease;
+  cursor: pointer;
+}
+.blue{
+  font-size:3rem ;
+}
+
+
 </style>
---
+++ src/components/Logo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      <img src="../assets/logo.png" alt="">
+      <p>{{ bienvenue||"Bienvenue !" }}</p>
+      <button @click="sentbounce"> Bounces with parent : {{bounces}}</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+    name: "Logo",
+    props: [
+        "bienvenue",
+        'bounces'],
+    
+    methods:{
+        sentbounce(){
+            this.$emit('sendBounce')
+        }
+
+    }
+}
+</script>
+
+<style scoped>
+    img{
+        height:300px
+    }
+
+</style>
